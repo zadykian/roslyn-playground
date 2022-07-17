71e337f62a961937c21ef62f0aeb6978122924d2
zadykian <zadykian@outlook.com>
2022-07-17 17:09:26 +0300

    initial commit

---
+++ src/RoslynPlayground.App/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.MSBuild;
+
+const string projectPath =
+	"/Users/zadykian/Repository/personal" +
+	"/postgres-marula/src/Postgres.Marula.DatabaseAccess/Postgres.Marula.DatabaseAccess.csproj";
+
+using var workspace = MSBuildWorkspace.Create();
+var project = await workspace.OpenProjectAsync(projectPath);
+// var projectCompilation = await project.GetCompilationAsync();
+
+foreach (var document in project.Documents)
+{
+	var syntaxRoot = await document.GetSyntaxRootAsync();
+	var syntaxTree = await document.GetSyntaxTreeAsync();
+
+	var semanticModel = await document.GetSemanticModelAsync();
+
+	var methodInvocations = syntaxRoot
+		.DescendantNodes()
+		.OfType<InvocationExpressionSyntax>()
+		.ToImmutableArray();
+}
